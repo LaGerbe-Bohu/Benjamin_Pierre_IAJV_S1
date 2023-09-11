15fe823d3023c683fca5d9e29e22e3b623909311
Benjamin <benjamin.gerbe@gmail.com>
2023-09-11 11:57:18 +0200

    intial commit

---
+++ Benjamin_Pierre_IAJV_S1/Benjamin_Pierre_IAJV_S1.cpp
@@ -0,0 +1,12 @@
+﻿// Benjamin_Pierre_IAJV_S1.cpp : Defines the entry point for the application.
+//
+
+#include "Benjamin_Pierre_IAJV_S1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
