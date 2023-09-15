ace2f1bf5fe734d9afa2f9c191f6e3208df5a53d
Pierre Durand <85309955+TotallyNotAB0t@users.noreply.github.com>
2023-09-15 14:44:25 +0200

    Time added

--- Benjamin_Pierre_IAJV_S1/Classes/src/Application.cpp
+++ Benjamin_Pierre_IAJV_S1/Classes/src/Application.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <iomanip>
 
 #include "../include/States.h"
 #include "../include/World.h"
@@ -50,6 +51,10 @@ void InitStates() {
 
 int main()
 {
+    clock_t start, end;
+    start = clock();
+
+    std::ios_base::sync_with_stdio(false);
 	//InitStates();
 
     #pragma region StatesInit
@@ -94,7 +99,7 @@ int main()
 
     MineGold.Action = ActionMineGold;
     MineGold.AddToVecPrecondition(&prepGold);
-    
+
 
     // Farm state ---------------
     States CreateFarm("Create Farm",Farm,3);
@@ -186,7 +191,7 @@ int main()
     world.SetBreadCount(20);
     world.SetWoodCount(10);
     world.SetIronCount(10);
-   
+
 
     // Goap machine hash map setup -------------
     gp.AddToHmap(Villager, { &CreateBread });
@@ -201,7 +206,7 @@ int main()
     gp.AddToHmap(Look, { &CreateVillager,&CreateWarrior });
 
 
-    Node* idx = gp.Execute(&AttackEnemy);
+    //Node* idx = gp.Execute(&AttackEnemy);
 
 	std::cout << " Bread : " << world.GetBreadCount() << std::endl;
 	std::cout << " Villager : " << world.GetVillagerCount() << std::endl;
@@ -211,9 +216,15 @@ int main()
 	std::cout << " Wood : " << world.GetWoodCount() << std::endl;
     std::cout << std::endl;
 
-    while (idx != nullptr) {
+    /*while (idx != nullptr) {
         std::cout << idx->GetState()->GetLabel() << std::endl;
         idx = idx->GetPrev();
-    }
+    }*/
+    end = clock();
+
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    std::cout << "Time taken by program is : " << std::fixed
+         << time_taken << std::setprecision(5);
+    std::cout << " sec " << std::endl;
     return 0;
 }
